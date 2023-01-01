b1bd8af19f98a832a234db49450096950397747e
zhoujingjin <5137845022@qq.com>
2023-01-01 17:35:31 +0800

    ruler

--- app/src/main/java/com/best/now/autoclick/utils/Constant.kt
+++ app/src/main/java/com/best/now/autoclick/utils/Constant.kt
@@ -7,16 +7,16 @@ date:2022/11/27
 class Constant {
     companion object {
         const val DOMAINAPP = "http://pdfexapi.wooribanj.info/"
-        const val EVENTDOMAIN = "http://temper.sephx.shop/"
-        const val APP_PARAMETER = "Temperature"
-        const val MIX_PARAMETER = "meessd1=vftzszs0&nbcgjj2=mwetd0&cvggd9=mxaszu1"
-        const val URL_PRIVACY_POLICY = "https://h73vhyaffj.feishu.cn/docx/Tm6GdxDwmoAEeZxSGDecofamngS"
-        const val URL_TERMS_OF_USE = "https://h73vhyaffj.feishu.cn/docx/HyzhdD3HHoh4WHx6eQucCD3Mnjd"
+        const val EVENTDOMAIN = "http://rulerpro.hnzhsm.cn/"
+        const val APP_PARAMETER = "Rulerpro"
+        const val MIX_PARAMETER = "xwesd0=erxdss1&nhyxdjj1=xeetd0&rtygd9=mxaszu0"
+        const val URL_PRIVACY_POLICY = "https://h73vhyaffj.feishu.cn/docx/U0oJd92OooEpV4xFiEpcVnesnUb"
+        const val URL_TERMS_OF_USE = "https://h73vhyaffj.feishu.cn/docx/FugXd11MeoPk4Lx8jNjc8C7NnHf"
         const val URL_QUALITY = "http://wendu.sephx.shop/wenduji/H5/mingqi-eng/air-quality.html"
         const val URL_HUMIDITY = "http://wendu.sephx.shop/wenduji/H5/mingqi-eng/humidity.html"
         const val URL_TEMPERATURE = "http://wendu.sephx.shop/wenduji/H5/mingqi-eng/temperature.html"
         const val URL_INDEX = "http://wendu.sephx.shop/wenduji/H5/mingqi-eng/index.html"
-        const val VIP_MONTH = "temperature_month"
+        const val VIP_MONTH = "ruler_month"
 
         //测试id
         const val AD_BANNER_ID = "ca-app-pub-3940256099942544/2934735716"
